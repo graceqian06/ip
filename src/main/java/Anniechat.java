de4083d3b830e9c068b7037206fb668200b940b6
grace qian <anyaqian16@gmail.com>
2026-08-21 10:31:34 +0800

    level 2 (add, list) done

--- src/main/java/Anniechat.java
+++ src/main/java/Anniechat.java
@@ -9,12 +9,22 @@ public class Anniechat {
         System.out.println(greet);
         System.out.println("What can I do for you?");
         Scanner scanner = new Scanner(System.in);
+        List<String> storage = new ArrayList<>();
+        int count = 0;
         while (true) {
             String echo = scanner.nextLine();
             if (echo.equals("bye")) {
                 System.out.println(exit);
-            } else {
-                System.out.println(echo);
+            }
+            else if (echo.equals("list")) {
+                for (String item: storage){
+                    System.out.println(item);
+                }
+            }
+            else {
+                System.out.println("added: "+echo);
+                count ++;
+                storage.add(count + "." + echo);
             }
         }
     }
