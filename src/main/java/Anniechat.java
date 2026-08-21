59419a258735efd1a7787fe9f790f859f9683c27
grace qian <anyaqian16@gmail.com>
2026-08-21 11:55:12 +0800

    implemented mark as done

--- src/main/java/Anniechat.java
+++ src/main/java/Anniechat.java
@@ -2,6 +2,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import static java.util.stream.IntStream.range;
+
 public class Anniechat {
     public static void main(String[] args) {
         String greet = "Hello! I'm Anniechat.";
@@ -10,22 +12,48 @@ public class Anniechat {
         System.out.println("What can I do for you?");
         Scanner scanner = new Scanner(System.in);
         List<String> storage = new ArrayList<>();
-        int count = 0;
+        List<Boolean> completed = new ArrayList<>();
         while (true) {
             String echo = scanner.nextLine();
             if (echo.equals("bye")) {
                 System.out.println(exit);
+                break;
             }
             else if (echo.equals("list")) {
-                for (String item: storage){
-                    System.out.println(item);
+                int len = storage.size();
+                System.out.println("a glimpse of ur tasks :)");
+                for (int i = 0; i < len; i ++){
+                    if (completed.get(i).equals(false)) {
+                        System.out.println(i+1 + ".[ ]" + storage.get(i));
+                    }
+                    else {
+                        System.out.println(i+1 + ".[x]" + storage.get(i));
+                    }
                 }
+            } else if (echo.startsWith("mark ")) {
+                System.out.println(" Nice! I've marked this task as done: \n");
+                int user_number = Integer.parseInt(echo.substring(5));
+                int sys_number = user_number -1;
+                completed.set(sys_number, true);
+                System.out.println("[x]" + storage.get(sys_number));
+
             }
             else {
                 System.out.println("added: "+echo);
-                count ++;
-                storage.add(count + "." + echo);
+                storage.add(echo);
+                completed.add(false);
             }
         }
     }
 }
+
+
+/*
+ else if (echo.startsWith("unmark ")) {
+                System.out.println(" OK, I've marked this task as not done yet: \n");
+                int user_number = Integer.parseInt(echo.substring(7));
+                int sys_number = user_number - 1;
+                completed.set(sys_number, false);
+                System.out.println("[ ]" + storage.get(sys_number));
+            }
+ */
