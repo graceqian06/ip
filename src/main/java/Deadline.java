b46eec3ccab326ee6ff26d759fcda7f93e2c9db3
grace qian <anyaqian16@gmail.com>
2026-08-27 17:15:57 +0800

    Add date parsing and formatting for deadlines

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,16 +1,21 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 public class Deadline extends Task{
     //example: deadline return book /by Sunday
-    String taskDesc;
-    String date;
-    String Desc;
+    private final LocalDate deadline;
+    private String taskOnly;
+    private String taskDesc;
+
     public Deadline(String desc) {
         super(desc);
-        String descDeadline = desc.substring(9);
-        Desc = descDeadline.split("/")[0];
-        String deadline = descDeadline.split("/")[1];
-        date = deadline.split(" ")[1];
-        taskDesc =  Desc + "(by: " + date + ")";
-    }
+        String descDeadline = desc.split(" ", 2)[1];
+        String[] parts  = descDeadline.split("/by ",2);
+        taskOnly = parts[0];
+        deadline = LocalDate.parse(parts[1]);
+        taskDesc = taskOnly + " (by: "
+                + deadline.format(OUTPUT_FORMAT) + ")";    }
     @Override
     public String getTaskDesc() {
         return taskDesc;
@@ -22,6 +27,9 @@ public class Deadline extends Task{
 
     @Override
     public String toSaveFormat(){
-        return this.getTaskIcon() + " | " + (isDone? "1" : "0") + " | " + this.Desc + " | " + this.date;
+        return this.getTaskIcon() + " | " + (isDone? "1" : "0") + " | " + this.taskOnly + " | " + this.deadline;
     }
+    private static final DateTimeFormatter OUTPUT_FORMAT =
+            DateTimeFormatter.ofPattern("MMM d yyyy", Locale.ENGLISH);
+
 }
